e0cd0a43ddb2559dfc2d7837e9075db5008e5032
Michał Woźniak <michal.wozniak86@int.pl>
2022-08-07 16:42:45 +0200

    another line of text

--- work1/src/Main.java
+++ work1/src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Kolejna linia tekstu");
+        System.out.println("Trzecia linia tekstu");
     }
 }
