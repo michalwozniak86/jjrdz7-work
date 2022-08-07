7525de953e7391bdcede0bc6872a4a755ef42a74
Michał Woźniak <michal.wozniak86@int.pl>
2022-08-07 16:38:39 +0200

    Revert "add author"
    This reverts commit 846b02f9482ebd9cb91b691730d6fa7415136714.

--- work1/src/Main.java
+++ work1/src/Main.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        String fullName = "Michał Woźniak";
-        System.out.println(fullName);
     }
 }
